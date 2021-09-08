c56f1520c9428c5f9b87e59954fee579b8438476
Emmanuel Atama <emmanuelatama@outlook.com>
2021-09-08 09:34:03 +0100

    Recent Update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,19 @@ export default function App() {
   const [hideForm, setHideForm] = useState(true);
 
   // [TODO] Write a useEffect to fetch contacts here...
+  useEffect(() => {
+    const url = `http://localhost:3030/contacts`;
+    console.log("Inside Url: ", url);
 
+    fetch(url)
+      .then((res) => res.json())
+      .then((contactsData) => {
+        console.log("Inside Get Fetch: ", contactsData);
+
+        setContacts(...contacts, contactsData);
+      });
+  }, []);
+  
   return (
     <>
       <ContactsList
